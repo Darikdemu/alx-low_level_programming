ecae63e942b0e3adf494e8603f0e42390f31cc57
DArik Demeke <darikdmsw@gmail.com>
2022-06-30 14:47:01 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <studio.h>
+#include <stdio.h>
 
 
 /**
- * main - Entry point 
- *
- * return: Always 0 (success)
- */
+* main - Entry point 
+*
+* return: Always 0 (success)
+*/
 int main(void)
 {
 puts("\"programming is like building a multilingual puzzel");
