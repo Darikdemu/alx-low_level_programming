cc01e8440cefe1d0929ef56823af4bc44b308f4b
DArik Demeke <darikdmsw@gmail.com>
2022-07-02 12:54:25 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,10 @@
 *
 * Return: Always 1 (Success)
 */
-
 int main(void)
 {
 
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 15);
+write(3, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 15);
 
 return (1);
 }
