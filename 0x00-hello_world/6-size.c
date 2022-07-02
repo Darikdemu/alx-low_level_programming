4fd3e9d16c9fe81209efa054d0758d886515fa10
DArik Demeke <darikdmsw@gmail.com>
2022-07-02 11:47:46 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ float f;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 
-rintf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 
