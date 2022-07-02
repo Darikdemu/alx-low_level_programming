515f2b8b0357a0b74f1506abc2729c5a23393e71
DArik Demeke <darikdmsw@gmail.com>
2022-07-02 12:29:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 /**
-* main - Entry point
+* main - prints the given text on the computer 
 * Return: Always 1 (Success)
-*
 */
 int main(void)
-
 {
 
-write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n");
+write("and that piece of art is useful - Dora Korpar, 2015-10-19");
 
 return (1);
 }
