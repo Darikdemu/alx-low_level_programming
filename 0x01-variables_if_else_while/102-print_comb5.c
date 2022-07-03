4f9b49d23256e8a3d8f78ab6e0beaa07490668c6
DArik Demeke <darikdmsw@gmail.com>
2022-07-03 13:55:17 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,9 +19,9 @@ int num1, num2;
 
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
-			putchar(' ');
 
-			if (num1 == 98 && num2 == 99)								continue;
+			if (num1 == 98 && num2 == 99)
+				continue;
 
 			putchar(',');
 			putchar(' ');
