bfc782f9023c99ee8d6de1f696bdcde5d30c08fe
DArik Demeke <darikdmsw@gmail.com>
2022-07-03 11:25:15 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ printf("Last Digit of %i is %i and is greater than %i\n", n, last_digit, 5);
 
 else if (last_digit == 0)
 {
-printf("Last Digit of %i is %i and %i\n", n, last_digit, 0);
+printf("Last Digit of %i is %i and is %i\n", n, last_digit, 0);
 }
 
 else if (last_digit < 6)
