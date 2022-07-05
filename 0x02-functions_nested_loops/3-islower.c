06248f5f0738dab2a2bd1b273b06e691f7b0643c
DArik Demeke <darikdmsw@gmail.com>
2022-07-05 11:47:37 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,18 @@
-#include "main.c"
+#include "main.h"
 /**
 * main - check the code.
-*
+* @c: takes in a character
 * Return: 1 for lowercase, 0 for uppercase .
 */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 
 
