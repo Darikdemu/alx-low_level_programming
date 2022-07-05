a2e7a51a57f238fa4b7f8ba481be505eacf7b61e
DArik Demeke <darikdmsw@gmail.com>
2022-07-05 10:32:29 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.c"
+/**
+* main - check the code.
+*
+* Return: 1 for lowercase, 0 for uppercase .
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+
+
+
+}
