e47a60d8ae5383afaf0f4dedd105b5c346f91cfe
DArik Demeke <darikdmsw@gmail.com>
2022-07-05 08:35:40 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.c"
+/**
+* main - check the code
+* Return: Always 0.
+*/
+
+void print_alphabet(void)
+{
+
+	    char c;
+	    for (c = a; c <=z; c++)
+		    _putchar(c);
+	    _putchar('\n');
+
+	       
+
+}
