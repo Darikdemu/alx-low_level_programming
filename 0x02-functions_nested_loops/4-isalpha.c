4dfbb575bd7d0b54d194fd9028959a2ce217c749
DArik Demeke <darikdmsw@gmail.com>
2022-07-05 15:25:04 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
 * _isalpha - checks for alphabetic character
 * @c: c is an ascii character
-* Return : 1 if c is letter, uppercase or lowercase otherwise 0
+* Return: 1 if c is a letter, 0 otherwise
 */
 
 int _isalpha(int c)
