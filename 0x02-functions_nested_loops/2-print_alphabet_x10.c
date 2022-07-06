ca7d65df06204a0dde4eeb36c996ea13718c2b17
DArik Demeke <darikdmsw@gmail.com>
2022-07-06 00:37:08 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
-* main - check the code.
-* Description : prints lowwercase alphabet
+*print_alphabet_x10 - prints alphabet letters
+* ten times
+*desc: The function uses loop to operate
+ *Return: no return
 */
 
 void print_alphabet_x10(void)
