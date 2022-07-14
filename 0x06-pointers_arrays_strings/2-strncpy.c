cd5deb2727d9c6804f11c54bce4c8826f7373603
DArik Demeke <darikdmsw@gmail.com>
2022-07-14 14:32:47 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,8 +11,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	
-	strncpy(dest, src, n);
+	for (i = 0; src[i] != '\0' && i < n; i++)
+
+		dest[i]	= src[i];
+
+		while (i < n)
+			dest[i++] = '\0';
 
 	return (dest);
 
