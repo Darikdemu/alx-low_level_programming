cf4dde71e4d61c1c2d35e75c60c959a6ea033952
DArik Demeke <darikdmsw@gmail.com>
2022-07-15 12:52:44 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,17 +4,25 @@
 * @dest: copy to
 * @src: copy from
 * @n: input number of char
-* Return: dest
+* Return: copy file
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+int i;
 
-	for (i = 0; src[i] != '\0'; i++)
-		if (i < n)
-			dest[i] = src[i];
-		while (i < n)
-			dest[i++] = '\0';
+
+for (i = 0; i < n; && src[i] != '\0'; i++)
+
+dest[i] = src[i];
+
+if (i < n)
+
+dest[i] = src[i];
+
+for (i = 0; i < n; i++)
+
+dest[i] = '\0';
+
 
 
 
