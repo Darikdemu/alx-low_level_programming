7a4b4b58301667c48491b922373a26493b1e1c93
DArik Demeke <darikdmsw@gmail.com>
2022-08-15 23:40:36 -0700

    sum list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,43 +1,15 @@
 #include "lists.h"
 /**
-* insert_nodeint_at_index - inserts a new node
-* at a given position.
-* @head: head of a list.
-* @idx: index of the list where the new node is
-* added.
-* @n: integer element.
-* Return: the address of the new node, or NULL if it
-* failed.
+* sum_listint - the sum of all the data (n) of a listint_t linked list
+* @head: first element
+* Return: the sum of the linked list
 */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+int sum_listint(listint_t *head)
 {
-	unsigned int i;
-
-	listint_t *new;
-	listint_t *h;
-	h = *head;
-	if (idx != 0)
-	{
-		for (i = 0; i < idx - 1 && h != NULL; i++)
-		{
-			h = h->next;
-		}
-	}
-	if (h == NULL && idx != 0)
-		return (NULL);
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	if (idx == 0)
-	{
-		new->next = *head;
-		*head = new;
-	}
+	if (head == NULL)
+		return (0);
+	if (head->next == NULL)
+		return (head->n);
 	else
-	{
-		new->next = h->next;
-		h->next = new;
-	}
-	return (new);
+		return (sum_listint(head->next) + head->n);
 }
